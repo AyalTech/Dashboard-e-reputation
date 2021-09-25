60519303fbabcc7c06a8d5272e80811c56dde95f
Ayal Haddad <haddad.ayale@gmail.com>
2021-09-26 00:36:12 +0200

    update

--- index.py
+++ index.py
@@ -1,76 +1,133 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+from app import app
 import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
+from panels import statistiques, recommandations, analyse_sentiment
 
+opportunities = pd.read_csv('C:/Users/Wizard/Desktop/M2 MLDS/PPD/dashboard/data/opportunity.csv', encoding='utf8')
+cases = pd.read_csv('C:/Users/Wizard/Desktop/M2 MLDS/PPD/dashboard/data/case.csv', encoding='utf8')
+leads = pd.read_csv('C:/Users/Wizard/Desktop/M2 MLDS/PPD/dashboard/data/lead.csv', encoding='utf8')
+server = app.server
 
-def prepare_daily_report():
+app.layout = html.Div(
+    [
+        html.Div(
+            className="row header",
+            children=[
+                html.Button(id="menu", children=dcc.Markdown("&#8801")),
+                html.Span(
+                    className="app-title",
+                    children=[
+                        dcc.Markdown("**Maquette PPD**"),
+                        html.Span(
+                            id="subtitle",
+                            children=dcc.Markdown("&nbsp Dashboard e-réputation"),
+                            style={"font-size": "1.8rem", "margin-top": "15px"},
+                        ),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            id="tabs",
+            className="row tabs",
+            children=[
+                dcc.Link("Statistiques", href="/"),
+                dcc.Link("Analyse de sentiment", href="/"),
+                dcc.Link("Recommandations", href="/"),
+            ],
+        ),
+        html.Div(
+            id="mobile_tabs",
+            className="row tabs",
+            style={"display": "none"},
+            children=[
+                dcc.Link("Statistiques", href="/"),
+                dcc.Link("Analyse de sentiment", href="/"),
+                dcc.Link("Recommandations", href="/"),
+            ],
+        ),
+        dcc.Store(  # opportunities df
+            id="opportunities_df",
+            data=opportunities.to_json(orient="split"),
+        ),
+        dcc.Store(  # leads df
+            id="leads_df",
+            data=leads.to_json(orient="split"),
+        ),
+        dcc.Store(
+            id="cases_df",
+            data=cases.to_json(orient="split"),
+        ),  # cases df
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="tab_content"),
+        html.Link(
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css",
+            rel="stylesheet",
+        ),
+        html.Link(
+            href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"
+        ),
+        html.Link(
+            href="https://fonts.googleapis.com/css?family=Open+Sans", rel="stylesheet"
+        ),
+        html.Link(
+            href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"
+        ),
+    ],
+    className="row",
+    style={"margin": "0%"},
+)
 
-    current_date = (datetime.today() - timedelta(days=1)).strftime('%m-%d-%Y')
-    app.logger.info(current_date);
-    df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + current_date + '.csv')
-    df_country = df.groupby(['Country_Region']).sum().reset_index()
-    df_country.replace('US', 'United States', inplace=True)
-    df_country.replace(0, 1, inplace=True)
-    
-    code_df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
-    df_country_code = df_country.merge(code_df, left_on='Country_Region', right_on='COUNTRY', how='left')
+# Update the index
 
-    df_country_code.loc[df_country_code.Country_Region == 'Congo (Kinshasa)', 'CODE'] = 'COD'
-    df_country_code.loc[df_country_code.Country_Region == 'Congo (Brazzaville)', 'CODE'] = 'COG'
-    
-    return(df_country_code)
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = 'Covid19-Dash'
-
-
-app.layout = html.Div([html.Div([html.H1("COVID19 Impact by Country")],
-                                style={'textAlign': "center", "paddingBottom": "30"}
-                               ),
-                       html.Div([html.Span("Metric to display : ", className="six columns",
-                                           style={"textAlign": "right", "width": "40%", "paddingTop": 10}),
-                                 dcc.Dropdown(id="value-selected", value='Confirmed',
-                                              options=[{'label': "Confirmed ", 'value': 'Confirmed'},
-                                                       {'label': "Recovered ", 'value': 'Recovered'},
-                                                       {'label': "Deaths ", 'value': 'Deaths'},
-                                                       {'label': "Active ", 'value': 'Active'}],
-                                              style={"display": "block", "marginLeft": "auto", "marginRight": "auto",
-                                                     "width": "70%"},
-                                              className="six columns")], className="row"),
-                       dcc.Graph(id="my-graph")
-                       ], className="container")
 
 @app.callback(
-    dash.dependencies.Output("my-graph", "figure"),
-    [dash.dependencies.Input("value-selected", "value")]
+    [
+        Output("tab_content", "children"),
+        Output("tabs", "children"),
+        Output("mobile_tabs", "children"),
+    ],
+    [Input("url", "pathname")],
 )
-def update_figure(selected):
-    #dff = prepare_confirmed_data()
+def display_page(pathname):
+    tabs = [
+        dcc.Link("Statistiques", href="/dash-salesforce-crm/statistiques"),
+        dcc.Link("Analyse de sentiment", href="/dash-salesforce-crm/analyse_sentiment"),
+        dcc.Link("Recommandations", href="/dash-salesforce-crm/recommandations"),
+    ]
+    if pathname == "/dash-salesforce-crm/statistiques":
+        tabs[0] = dcc.Link(
+            dcc.Markdown("**&#9632 Statistiques**"),
+            href="/dash-salesforce-crm/statistiques",
+        )
+        return statistiques.layout, tabs, tabs
+    elif pathname == "/dash-salesforce-crm/recommandations":
+        tabs[2] = dcc.Link(
+            dcc.Markdown("**&#9632 Recommandations**"), href="/dash-salesforce-crm/recommandations"
+        )
+        return recommandations.layout, tabs, tabs
+    tabs[1] = dcc.Link(
+        dcc.Markdown("**&#9632 Analyse de sentiment**"), href="/dash-salesforce-crm/analyse_sentiment"
+    )
+    return analyse_sentiment.layout, tabs, tabs
 
-    dff = prepare_daily_report()
-    dff['hover_text'] = dff["Country_Region"] + ": " + dff[selected].apply(str)
 
-    trace = go.Choropleth(locations=dff['CODE'],z=np.log(dff[selected]),
-                          text=dff['hover_text'],
-                          hoverinfo="text",
-                          marker_line_color='white',
-                          autocolorscale=False,
-                          reversescale=True,
-                          colorscale="RdBu",marker={'line': {'color': 'rgb(180,180,180)','width': 0.5}},
-                          colorbar={"thickness": 10,"len": 0.3,"x": 0.9,"y": 0.7,
-                                    'title': {"text": 'persons', "side": "bottom"},
-                                    'tickvals': [ 2, 10],
-                                    'ticktext': ['100', '100,000']})   
-    return {"data": [trace],
-            "layout": go.Layout(height=800,geo={'showframe': False,'showcoastlines': False,
-                                                                      'projection': {'type': "miller"}})}
+@app.callback(
+    Output("mobile_tabs", "style"),
+    [Input("menu", "n_clicks")],
+    [State("mobile_tabs", "style")],
+)
+def show_menu(n_clicks, tabs_style):
+    if n_clicks:
+        if tabs_style["display"] == "none":
+            tabs_style["display"] = "flex"
+        else:
+            tabs_style["display"] = "none"
+    return tabs_style
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=False)
